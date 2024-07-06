d2cbc3b600f8c00128f1192b9ed11d5122bf3dba
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-07-06 19:07:54 +0800

    refactor: ground up boiler

---
+++ resources/js/components/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+	return <>Hi I'm Kurt</>;
+};
+
+export default App;
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>React    </title>
+    <title>Document</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
+@vite('resources/css/app.css')
+
 <body>
-@viteReactRefresh
-@vite('resources/js/app.jsx')
+    <div id="root"></div>
+
+        @viteReactRefresh
+        @vite('resources/js/app.ts')
+        
 </body>
+
+<script>
+    window.env = {
+        API_BASE_URL: '{{ env("API_BASE_URL") }}'
+    }
+</script>
+
 </html>
