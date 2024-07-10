133d4d0277f5cf1a22e0d77d8ef027fc165676cb
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-07-10 23:30:27 +0800

    refactor: migrated task table

--- database/migrations/2024_07_09_141901_create_tasks_table.php
+++ database/migrations/2024_07_09_141901_create_tasks_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->text('description');
             $table->boolean('completed')->default(false);
             $table->timestamps();
+            
         });
     }
 
